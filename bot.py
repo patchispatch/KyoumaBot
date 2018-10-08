ca9ad60087bd284cf8c67ade09aac750286b47f3
patchispatch <juanov98@gmail.com>
2018-10-08 20:15:47 +0200

    Add start conversation

--- bot.py
+++ bot.py
@@ -2,7 +2,9 @@
 
 import logging
 from config import TOKEN
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+						  ConversationHandler, RegexHandler)
+from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
 import utils
 
 
@@ -14,12 +16,104 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+###############################################################################
+# Initial conversation
 
+# States:
+GENDER, PHOTO, LOCATION, BIO = range(4)
+
+# Functions:
 def start(bot, update):
-	message = 'Saludos, {}. Conocerte es la elección de la Steins Gate. El. Psy. Kongroo.'. \
-				format(update.message.from_user.first_name)
-	update.message.reply_text(message)
+	reply_keyboard = [["Chica", "Chico", "Otro"]]
 
+	update.message.reply_text(
+		"Hola. Va a sonar a profesor pokémon, pero... ¿qué eres exactamente? "
+		"Si quieres terminar la conversación, usa /cancel.",
+		reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+	return GENDER
+
+def gender(bot, update):
+	user = update.message.from_user
+	logger.info("Género de %s: %s", user.first_name, update.message.text)
+	update.message.reply_text(
+		"Bien, bien. Ahora, mándame una foto de un gato. Confío en ti. "
+		"Si no quieres, escribe /skip y ya.")
+
+	return PHOTO
+
+def photo(bot, update):
+	user = update.message.from_user
+	photo_file = bot.get_file(update.message.photo[-1].file_id)
+	photo_file.download('photos/{}.jpg'.format(user.first_name))
+	logger.info("Foto de %s: %s", user.first_name, 'user_photo.jpg')
+	update.message.reply_text(
+		"Voy a confiar en que me has enviado una foto de un gato. Ahora "
+		"sólo necesito que me envíes tu ubicación. Si prefieres no decirlo, escribe "
+		"/skip y permanecerá en el anonimato.")
+
+	return LOCATION
+
+def location(bot, update):
+	user = update.message.from_user
+	user_location = update.message.location
+	logger.info("Location of %s: %f / %f", user.first_name, user_location.latitude,
+				user_location.longitude)
+	update.message.reply_text("Parece un buen punto de encuentro, estás a salvo "
+								"de la organización. Ahora, cuéntame algo sobre ti. "
+								"Si no quieres, /skip.")
+
+	return BIO
+
+
+def skip_photo(bot, update):
+	user = update.message.from_user
+	logger.info("Usuario %s no envió foto.", user.first_name)
+	update.message.reply_text(
+		"Bueno, entiendo que no te fíes de mí como para mandarme fotos de "
+		"gatos, pero al menos, cuéntame algo sobre ti. "
+		"Si no quieres, ya sabes, /skip.")
+
+	return LOCATION
+
+def skip_location(bot, update):
+	user = update.message.from_user
+	logger.info("User %s did not send a location.", user.first_name)
+	update.message.reply_text(
+		"Hm, así que no quieres revelar tus datos... Es comprensible. "
+ 		"La organización podría estar escuchando. "
+		"Si quieres contarme algo sobre ti, adelante. "
+		"Aunque puedes negarte con /skip.")
+
+	return BIO
+
+
+def skip_bio(bot, update):
+	user = update.message.from_user
+	logger.info("User %s did not send a location.", user.first_name)
+
+	update.message.reply_text(
+		"Haces bien guardando silencio... Quién sabe si la Organización\
+		nos está escuchando. Nos veremos, {}. El. Psy. Kongroo.".format(user.first_name))
+
+	return ConversationHandler.END
+
+def bio(bot, update):
+	user = update.message.from_user
+	logger.info("Bio of %s: %s", user.first_name, update.message.text)
+	update.message.reply_text(
+		"Gracias por tu tiempo. Me aseguraré de mantener esta información "
+		"lejos de las manos de la organización. El. Psy. Kongroo.")
+
+def cancel(bot, update):
+	user = update.message.from_user
+	logger.info("User %s canceled the conversation.", user.first_name)
+	update.message.reply_text("Nos vemos, {}. El. Psy. Kongroo".format(user.first_name),
+								reply_markup=ReplyKeyboardRemove())
+
+	return ConversationHandler.END
+
+###############################################################################
 # Methods without command:
 
 # Detecta si alguien se refiere a alumnos en vez de a estudiantes
@@ -63,6 +157,28 @@ def main():
 
 	# Have the dispatcher register the handlers:
 	dp = updater.dispatcher
+
+	# Conv. handler: start.
+	conv_handler = ConversationHandler(
+		entry_points=[CommandHandler('start', start)],
+
+		states={
+			GENDER: [RegexHandler('^(Chica|Chico|Otro)$', gender)],
+
+			PHOTO: [MessageHandler(Filters.photo, photo),
+					CommandHandler('skip', skip_photo)],
+
+			LOCATION: [MessageHandler(Filters.location, location),
+						CommandHandler('skip', skip_location)],
+
+			BIO: [MessageHandler(Filters.text, bio),
+						CommandHandler('skip', skip_bio)]
+			},
+
+		fallbacks=[CommandHandler('cancel', cancel)]
+	)
+
+	dp.add_handler(conv_handler)
 
 	# Commands:
 	dp.add_handler(CommandHandler('start', start))
